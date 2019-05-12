13eff1259e26ada9cbee14fd135821ed87dcb6f3
Luthfi Naufal <Luthfi Naufal@DESKTOP-6S87NTJ>
2019-05-12 19:46:18 +0700

    Update header.php

--- LP01_1700654_Muhammad Luthfi Naufal/application/views/backend/header.php
+++ LP01_1700654_Muhammad Luthfi Naufal/application/views/backend/header.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Kereta</title>
+    <title>Kereta TES 123</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
   <title></title>
   <style>
